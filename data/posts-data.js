d6ed2189fc5216950246467d2bb10d14dd49d48b
ningningwang <470613423@qq.com>
2017-01-21 20:28:39 +0800

    git

---
+++ data/posts-data.js
@@ -0,0 +1,26 @@
+var local_database = [
+    {
+        date: "Sep 18 2016",
+        title: "正是虾肥蟹美时",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时",
+        reading: "112",
+        collection: "65",
+        postId:0
+    },
+    {
+        date: "Sep 18 2016",
+        title: "正是虾肥蟹美时",
+        imgSrc: "/images/post/bl.png",
+        avatar: "/images/avatar/2.png",
+        content: "正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时正是虾肥蟹美时",
+        reading: "112",
+        collection: "65",
+        postId:1    
+    }
+]
+
+module.exports = {
+    postList:local_database
+}
---
+++ pages/posts/post.js
@@ -0,0 +1,25 @@
+var postsData = require('../../data/posts-data.js')
+
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    // this.setData.postList = postsData.postList
+
+    // this.setData({
+    //     posts_key:postsData.postList
+    // });
+
+    this.setData({
+      postList: postsData.postList
+    });
+  },
+  onPostTap: function (event) {
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url:"post-detail/post-detail"
+    })
+  }
+})
