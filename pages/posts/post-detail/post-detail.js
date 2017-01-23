5e07396ace0793bad5970bb95199af06fd65d85d
ningningwang <470613423@qq.com>
2017-01-23 21:03:37 +0800

    6.4

---
+++ pages/posts/post-detail/post-detail.js
@@ -0,0 +1,11 @@
+var postsData = require('../../../data/posts-data.js')
+
+Page({
+    onLoad:function(option){ 
+        var postId = option.id;
+        var postData = postsData.postList[postId];
+        this.setData({ 
+            postData : postData
+        })
+    } 
+}) 
