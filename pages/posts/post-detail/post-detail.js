8c02e58b37e91ca97d83680007b0381866d3dce3
ningningwang <470613423@qq.com>
2017-01-24 20:25:54 +0800

    6.5

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,11 +1,23 @@
 var postsData = require('../../../data/posts-data.js')
 
 Page({
-    onLoad:function(option){ 
+    onLoad:function(option){  
         var postId = option.id;
         var postData = postsData.postList[postId];
         this.setData({ 
             postData : postData
         })
-    } 
+        wx.setStorageSync('key', {
+            game:"风暴英雄",
+            developer:"暴雪"
+        })
+    }, 
+    onCollectionTap:function(event){
+        var game = wx.getStorageSync('key')
+    },
+    onShareTap:function(event){
+        wx.clearStorageSync();
+    }
+
+
 }) 
