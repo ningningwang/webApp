8e91033f22b95433480596df45fe6696ecb9c004
ningningwang <470613423@qq.com>
2017-01-28 19:08:31 +0800

    音乐

--- data/posts-data.js
+++ data/posts-data.js
@@ -11,7 +11,12 @@ var local_database = [
         author:"梦婷淘金",
         dateTime:"24小时前",
         detail:"近期，腾讯公布了2016年业绩报告。而微信无疑成为外界最为关注的焦点，截止今年第一季度末，微信每月活跃用户已达到5.49亿，用户覆盖200多个国家、超过20种语言。还有一个比较有意思的数据是，微信用户的男女比例为1.8：1，男性用户约占了64.3%，而女性用户则只有35.7%，用户整体以男性为主。不过，为何我总是感觉微信上的女性用户更多呢？相对于QQ、微博等社交应用，微信不仅在功能上比较简化，也没有让过多的广告影响到用户体验，而且在开机画面上保持了“数十年如一日”的专注。那么，为什么微信要一直用下面这幅开机画面呢？",
-        postId:0
+        postId:0,
+        music:{
+            url:"http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+            title:"朋友",
+            coverImg:"http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
     },
     {
         date: "Sep 18 2016",
@@ -25,7 +30,12 @@ var local_database = [
         author:"杭州聚拢",
         dateTime:"24小时前",
         detail:"找APP开发团队一般有APP开发公司，APP开发团队，个人APP开发。一般情况下找个人开发app费用要相对小点，但是选择个人开发项目质量不能得到有效保证，个人能力比较局限，对于后期上线维护也很难做的比较全面！团队要比个人的成本高些，由于团队合作其项目经验和综合能力比起个人来都强很多，但是团队的人员配置肯定不及公司，所以理想程度肯定是不如专业app开发公司！",
-        postId:1    
+        postId:1,   
+        music:{
+            url:"http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+            title:"女儿情",
+            coverImg:"http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
     }
 ]
 
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,23 +1,140 @@
 var postsData = require('../../../data/posts-data.js')
 
 Page({
-    onLoad:function(option){  
-        var postId = option.id;
-        var postData = postsData.postList[postId];
-        this.setData({ 
-            postData : postData
-        })
-        wx.setStorageSync('key', {
-            game:"风暴英雄",
-            developer:"暴雪"
-        })
-    }, 
-    onCollectionTap:function(event){
-        var game = wx.getStorageSync('key')
+    data: {
+        isPlayingMusic: false
     },
-    onShareTap:function(event){
-        wx.clearStorageSync();
+    onLoad: function (option) {
+        var postId = option.id;
+        this.data.currentPostId = postId;
+        var postData = postsData.postList[postId];
+        this.setData({
+            postData: postData
+        })
+        var postsCollected = wx.getStorageSync('posts_collected')
+        if (postsCollected) {
+            var postCollected = postsCollected[postId]
+            this.setData({
+                collected: postCollected
+            })
+        }
+        else {
+            var postsCollected = {};
+            postsCollected[postId] = false;
+            wx.setStorageSync('posts_collected', postsCollected);
+        }
+    },
+
+    onColletionTap: function (event) {
+        // this.getPostsCollectedAsy();
+        this.getPostsCollectedSyc();
+    },
+
+    //异步
+    getPostsCollectedAsy: function () {
+        var that = this;
+        wx.getStorage({
+            key: 'posts_collected',
+            success: function (res) {
+                var postsCollected = wx.getStorageSync('posts_collected');
+                var postCollected = postsCollected[that.data.currentPostId];
+                //取反，收藏变成未收藏，未收藏变成收藏
+                postCollected = !postCollected;
+                postsCollected[that.data.currentPostId] = postCollected;
+                that.showToast(postsCollected, postCollected);
+            }
+        })
+    },
+    //同步
+    getPostsCollectedSyc: function () {
+        var postsCollected = wx.getStorageSync('posts_collected');
+        var postCollected = postsCollected[this.data.currentPostId];
+        //取反，收藏变成未收藏，未收藏变成收藏
+        postCollected = !postCollected;
+        postsCollected[this.data.currentPostId] = postCollected;
+        this.showToast(postsCollected, postCollected);
+    },
+
+
+    showModal: function (postsCollected, postCollected) {
+        var that = this;
+        wx.showModal({
+            title: "收藏",
+            content: postCollected ? "收藏该文章?" : "取消收藏?",
+            showCancel: "true",
+            cancelText: "取消",
+            cacelColor: "#333",
+            confirmText: "确认",
+            confirmColor: "#405f80",
+            success: function (res) {
+                if (res.confirm) {
+                    //更新文章是否的缓存值
+                    wx.setStorageSync('posts_collected', postsCollected);
+                    //更新数据绑定变量，从而实现切换图片
+                    that.setData({
+                        collected: postCollected
+                    })
+                }
+            }
+        })
+    },
+
+    showToast: function (postsCollected, postCollected) {
+        //更新文章是否的缓存值
+        wx.setStorageSync('posts_collected', postsCollected);
+        //更新数据绑定变量，从而实现切换图片
+        this.setData({
+            collected: postCollected
+        })
+        wx.showToast({
+            title: postCollected ? "收藏成功" : "取消成功",
+            duration: 1000,
+            icon: "success"
+        })
+    },
+
+    //分享
+    onShareTap: function () {
+        var itemList = [
+            "分享到朋友圈",
+            "分享到微信好友",
+            "分享到qq",
+            "分享到微博"
+        ]
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: "405f80",
+            success: function (res) {
+                wx.showModal({
+                    title: "用户" + itemList[res.tapIndex],
+                    content: "用户是否取消" + res.cancel + "现在无法实现分享功能。"
+                })
+            }
+        })
+    },
+    //音乐
+    onMusicTap: function (event) {
+        var currentPostId = this.data.currentPostId;
+        var postData = postsData.postList[currentPostId];
+        var isPlayingMusic = this.data.isPlayingMusic;
+        if (isPlayingMusic) {
+            wx.pauseBackgroundAudio();
+            this.setData({
+                isPlayingMusic:false
+            })
+        }
+        else{
+            wx.playBackgroundAudio({
+              dataUrl: postData.music.url,
+              title:postData.music.title,
+              coverImgUrl:postData.music.coverImgUrl,
+            })
+            this.setData({
+                isPlayingMusic:true
+            })
+        }
     }
+
 
 
 }) 
