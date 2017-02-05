fc4e28e307a73484e90ceb3954313071740263ea
ningningwang <470613423@qq.com>
2017-02-05 22:05:01 +0800

    电影更多页面加载

--- utils/util.js
+++ utils/util.js
@@ -1,18 +1,34 @@
 // [1,1,1,0,0]
-function convertToStarsArray(stars){
-    var num = stars.toString().substring(0,1);
+function convertToStarsArray(stars) {
+    var num = stars.toString().substring(0, 1);
     var array = [];
-    for(var i = 1;i <= 5;i++){
-        if(i<=num){
+    for (var i = 1; i <= 5; i++) {
+        if (i <= num) {
             array.push(1);
         }
-        else{
+        else {
             array.push(0);
         }
     }
     return array;
 }
+function http(url, callBack) {
+    wx.request({
+        url: url,
+        method: 'GET', 
+        header: {
+            "Content-Type": "json"
+        }, // 设置请求的 header
+        success: function (res) {
+            callBack(res.data);
+        },
+        fail: function (error) {
+            console.log(error)
+        }
+    })
+}
 
 module.exports = {
-    convertToStarsArray:convertToStarsArray
+    convertToStarsArray: convertToStarsArray,
+    http:http
 }
